73cf6988fadc1b609e523738ab907b23bf537328
Noud de Brouwer <noud4@home.nl>
2020-03-24 21:47:45 +0100

    IconButton dev

--- src/components/EntityTable/CellRenderers/ActionButton.tsx
+++ src/components/EntityTable/CellRenderers/ActionButton.tsx
@@ -5,6 +5,9 @@ import Button from '@material-ui/core/Button';
 import Tooltip from '@material-ui/core/Tooltip';
 import defaultActions from '../../../lib/defaultActions';
 
+import IconButton from '@material-ui/core/IconButton';
+import { Delete, Edit } from '@material-ui/icons';
+
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     actionButton: {
@@ -12,6 +15,35 @@ const useStyles = makeStyles((theme: Theme) =>
     },
   })
 );
+
+
+const TextButton = (actionButton: any, handleClick: any, actionType: any) => {
+  // const { actionButton, handleClick, actionType } = props;
+  return (
+    <Button variant="contained" className={actionButton} onClick={handleClick}>
+     {actionType}
+    </Button>
+  );
+} 
+
+const DeleteButton = (actionButton: any, handleClick: any, actionType: any) => {
+  // const { actionButton, handleClick, actionType } = props;
+  return (
+    <IconButton aria-label="delete" variant="contained" className={classes.actionButton} onClick={handleClick}>
+     <Delete />
+    </IconButton>
+  );
+}
+
+
+const EditButton = (actionButton: any, handleClick: any, actionType: any) => {
+  // const { actionButton, handleClick, actionType } = props;
+  return (
+    <IconButton aria-label="edit" variant="contained" className={classes.actionButton} onClick={handleClick}>
+     <Edit />
+    </IconButton>
+  );
+}
 
 const ActionButton = (props: any) => {
   const { actionType, entityName, entityId, actions } = props;
@@ -24,11 +56,24 @@ const ActionButton = (props: any) => {
     defaultActions[actionType](entityName, entityId, { history, actions });
   }
 
+  console.log('actionType',actionType);
+
+  var buttonToRender = TextButton;
+  switch(actionType) {
+    case "delete":
+        buttonToRender = DeleteButton;
+        break;
+      case "edit":
+        buttonToRender = EditButton;
+        break;
+    }
+
   return (
     <Tooltip title={actionType}>
-      <Button variant="contained" className={classes.actionButton} onClick={handleClick}>
-        {actionType}
-      </Button>
+      <div>
+        {TextButton(classes.actionButton, handleClick, actionType)}
+        {buttonToRender}
+      </div>
     </Tooltip>
   );
 };
--- src/components/EntityTable/CellRenderers/ActionButtons.tsx
+++ src/components/EntityTable/CellRenderers/ActionButtons.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ActionButton from './ActionButton';
 
+import IconButton from '@material-ui/core/IconButton';
+import { Delete, Edit } from '@material-ui/icons';
+
 export default function ActionButtons(props) {
   const { row = [], data = [], initialState = {} } = props;
 
@@ -9,9 +12,12 @@ export default function ActionButtons(props) {
 
   return (
     <React.Fragment>
-      <ActionButton key="view" entityId={id} entityName={entityName} actionType="view" />
-      <ActionButton key="edit" entityId={id} entityName={entityName} actionType="edit" />
-      <ActionButton key="delete" entityId={id} entityName={entityName} actionType="delete" actions={actions} />
+      <IconButton aria-label="edit" key="edit" entityId={id} entityName={entityName} actionType="edit" actions={actions}>
+        <Edit />
+      </IconButton>
+      <IconButton aria-label="delete" key="delete" entityId={id} entityName={entityName} actionType="delete" actions={actions}>
+        <Delete />
+      </IconButton>
     </React.Fragment>
   );
 }
